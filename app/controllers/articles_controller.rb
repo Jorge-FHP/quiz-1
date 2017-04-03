dfd900d06f52c7eaa74b466dfcfae11d866a017b
Jorge-FHP <jnandez565@gmail.com>
2017-04-03 02:15:42 +0000

    Setting up a database

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+  def  index
+    @article = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description, :image)
+  end
+
+end
